96f21d43dff8ed656de67ce40a92791235415a27
Jesus-ASG <jesus.serrano.graciano@gmail.com>
2023-02-07 03:43:13 -0600

    added styles for home page

--- main/controllers/views.py
+++ main/controllers/views.py
@@ -9,10 +9,12 @@ def index(request):
         return render(request, 'no-logged/home.html')
     
     page_title = 'Stories Gallery'
+    message_if_empty = ''
     stories = Story.objects.all()
     context = {
         'stories': stories,
         'page_title': page_title,
+        'message_if_empty': message_if_empty,
     }
     return render(request, 'user/index_user.html', context)
     
@@ -21,11 +23,13 @@ def index(request):
 def savedStories(request):
     user_profile = request.user.profile
     page_title = 'Your saved stories'
+    message_if_empty = "You don't have saved stories yet"
     stories = Story.objects.filter(savedstory__user_profile=user_profile).order_by('-savedstory__date')
 
     context = {
         'page_title': page_title,
         'stories': stories,
+        'message_if_empty': message_if_empty,
     }
     return render(request, 'user/index_user.html', context)
 
@@ -33,12 +37,14 @@ def savedStories(request):
 @login_required(login_url='/login/')
 def likedStories(request):
     user_profile = request.user.profile
-    page_title = 'Your favorites stories'
+    page_title = 'Your favorite stories'
+    message_if_empty = "You don't have favorite stories yet"
     stories = Story.objects.filter(likedstory__user_profile=user_profile).order_by('-likedstory__date')
 
     context = {
         'page_title': page_title,
         'stories': stories,
+        'message_if_empty': message_if_empty,
     }
     return render(request, 'user/index_user.html', context)
 
