5e2680e77b1fb17c172ead89cdd554f1f943391e
Jesus-ASG <jesus.serrano.graciano@gmail.com>
2023-03-03 15:42:55 -0600

    Scripts to filter tags button
    Added functionality of selection for checkbox, including 'all' button

--- main/static/js/utils/dropdown.js
+++ main/static/js/utils/dropdown.js
@@ -1,20 +1,96 @@
-const items = document.querySelectorAll(".item");
+const menu_items_list = document.getElementsByClassName("menu-items");
 
-items.forEach(item => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("checked");
-        let check_input = document.getElementById(item.getAttribute('target'));
-        if (check_input)
-            check_input.checked = !check_input.checked;
+for (let menu_items of menu_items_list) {
+	let items = menu_items.getElementsByClassName("item");
+	let dom_select_all = null;
 
+	for (let item of items) {
+		let x = item.getAttribute("select-all");
+		if (x == "true") {
+			dom_select_all = item;
 
-        let checked = document.querySelectorAll(".checked"),
-            btnText = document.querySelector(".btn-text");
+			let select_all = item.getAttribute("select-all");
+			if (select_all == "true") {
+				item.addEventListener("click", () => {
+					if (item.classList.contains("checked")) {
+						for (let i of items)
+							if (i.classList.contains("checked")) {
+								clickCheckbox(i);
+							};
+					} else {
+						for (let i of items)
+							if (!i.classList.contains("checked")) {
+								clickCheckbox(i);
+							};
+					}
+					showLabelMessage(menu_items);
+				});
+			}
+		} else {
+			item.addEventListener("click", () => {
+				let input = item.querySelector("input");
+				if (input) {
+					let input_type = input.getAttribute("type");
 
-            if(checked && checked.length > 0){
-                btnText.innerText = `${checked.length} Selected`;
-            }else{
-                btnText.innerText = "Select Language";
-            }
-    });
-})
+					switch (input_type) {
+						case "checkbox":
+							clickCheckbox(item);
+							showLabelMessage(menu_items);
+							if (dom_select_all) {
+								let checked = menu_items.querySelectorAll(".checked");
+								if (checked.length < items.length) {
+									if (checked.length == items.length - 1 && !dom_select_all.classList.contains("checked")) {
+										dom_select_all.classList.add("checked");
+										let check_input = document.getElementById(item.getAttribute("target"));
+										if (check_input)
+											check_input.checked = true;
+									}
+									else {
+										dom_select_all.classList.remove("checked");
+										let check_input = document.getElementById(item.getAttribute("target"));
+										if (check_input)
+											check_input.checked = false;
+									}
+								}
+							}
+							break;
+
+						case "radio":
+							console.log("is radio");
+					}
+				}
+			});
+		}
+	}
+}
+
+/*
+function clearSelection(item){
+		item.classList.remove('checked');
+		let check_input = 
+}
+*/
+
+function clickCheckbox(item) {
+	item.classList.toggle("checked");
+	let check_input = document.getElementById(item.getAttribute("target"));
+	if (check_input)
+		check_input.checked = !check_input.checked;
+}
+
+function showLabelMessage(menu_items) {
+	let checked = menu_items.querySelectorAll(".checked");
+	const label_id = menu_items.getAttribute("label-id");
+	const default_label = menu_items.getAttribute("default-label");
+
+	if (label_id && default_label) {
+		let label = document.getElementById(label_id);
+		if (label) {
+			if (checked && checked.length > 0) {
+				label.innerText = `${checked.length} Selected`;
+			} else {
+				label.innerText = default_label + "";
+			}
+		}
+	}
+}
