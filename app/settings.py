4d349db6ca2b498ee5736794a71cc52a70805930
Jesus-ASG <jesus.serrano.graciano@gmail.com>
2023-10-23 23:21:09 -0600

    Added .env configuration

--- app/settings.py
+++ app/settings.py
@@ -1,27 +1,20 @@
 from pathlib import Path
 import os
 
-#import mimetypes
-#mimetypes.add_type("text/css", ".css", True)
+from dotenv import load_dotenv
+load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e0e+1u8g7v-wga7@89@y#fv=(v9@4otn6db1x(n^!shz(w7#bx'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = eval(os.getenv('DEBUG'))
 
 ALLOWED_HOSTS = ['3.130.185.169', 'localhost', '127.0.0.1']
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'daphne',
     'django.contrib.admin',
@@ -78,14 +71,12 @@ CHANNEL_LAYERS = {
 }
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'story_quest_db',
-        'USER': 'root',
-        'PASSWORD': 'SQDBP4ssword.',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
@@ -119,21 +110,13 @@ CACHES = {
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = 'es-mx'
-
 TIME_ZONE = 'America/Mexico_City'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 STATIC_URL = '/static/'
 
@@ -144,6 +127,4 @@ AUDIOS_PATH = 'uploads/audios/%Y/%m/%d/'
 
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
